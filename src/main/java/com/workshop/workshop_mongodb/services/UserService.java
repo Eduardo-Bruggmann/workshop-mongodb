2a6e41e109f2eae38a025af869cee52d1cef054f
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-03-05 14:57:29 -0300

    Getting user by id

--- src/main/java/com/workshop/workshop_mongodb/services/UserService.java
+++ src/main/java/com/workshop/workshop_mongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.workshop.workshop_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.workshop_mongodb.domain.User;
 import com.workshop.workshop_mongodb.repository.UserRepository;
+import com.workshop.workshop_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> u = repo.findById(id);
+		return u.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
