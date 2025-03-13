8e321c5f139c8fb0c9c8e4201c63882658868331
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-03-13 16:11:07 -0300

    Simple queries with query methods

--- src/main/java/com/workshop/workshop_mongodb/services/PostService.java
+++ src/main/java/com/workshop/workshop_mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.workshop.workshop_mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> p = repo.findById(id);
 		return p.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
